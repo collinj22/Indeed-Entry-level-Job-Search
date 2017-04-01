c98a5fd2d74b7f4fc88605c64f61f29330bcf3ba
collin <collinj22@gmail.com>
2017-04-01 11:22:28 -0400

    Formatting

--- Indeed_Job_Search_Bokeh/main.py
+++ Indeed_Job_Search_Bokeh/main.py
@@ -1,7 +1,7 @@
 from os.path import dirname, join
 
 import pandas as pd
-from bokeh.layouts import row, widgetbox, column
+from bokeh.layouts import row, widgetbox, column, layout
 from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.models.widgets import Slider, Button, DataTable, TableColumn, NumberFormatter, TextInput
 from bokeh.io import curdoc
@@ -14,9 +14,7 @@ source = ColumnDataSource(df)
 def search():
     data_table.columns = [TableColumn(field="name", title="Loading...")]
     df = IndeedSearch.main(query.value, location.value, radius.value)
-    column_names = [
-        TableColumn(field=c, title=c) for c in df.columns.values
-        ]
+    column_names = [TableColumn(field=c, title=c) for c in df.columns.values]
     data_table.columns = column_names
     data_table.source = ColumnDataSource(df)
     return
@@ -24,18 +22,14 @@ def search():
 
 columns = [TableColumn(field="name", title="Job Search")]
 data_table = DataTable(source=source, columns=columns, width=1100, height=500)
-query = TextInput(title="Job Title", value='')
-location = TextInput(title="Location", value='')
-radius = TextInput(title="Radius", value='')
-search_button = Button(label="Search", button_type="success", width=180)
+query = TextInput(title="Job Title", value='', width=200)
+location = TextInput(title="Location", value='', width=200)
+radius = TextInput(title="Radius", value='', width=200)
+search_button = Button(label="Search", button_type="success", width=200)
 search_button.on_click(search)
-download_button = Button(label="Download", button_type="success", width=180)
+download_button = Button(label="Download", button_type="success", width=200)
 download_button.callback = CustomJS(args=dict(source=data_table.source), code='download.js')
 
-
-controls = widgetbox(search_button, download_button)
-table = widgetbox(data_table)
-text = widgetbox(query, location, radius)
-left_column = column(text, controls)
-curdoc().add_root(row(left_column, table))
+control_row = row(query, location, radius, search_button)
+curdoc().add_root(row(control_row, data_table))
 curdoc().title = "Indeed Job Search"
